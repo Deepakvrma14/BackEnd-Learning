23c060c2231b7f699e94178c4b544c6f4ff6cd17
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-10-14 02:09:26 +0530

    added 3 files

---
+++ NodeJS/server.js
@@ -0,0 +1,18 @@
+console.log('Hello World!')
+// console.log(global)
+const os = require('os')
+const path = require('path')    
+
+console.log(os.platform())
+console.log(os.version())
+console.log(os.homedir())
+
+console.log(__dirname)
+console.log(__filename)
+
+
+console.log(path.basename(__filename))
+console.log(path.dirname(__filename))
+console.log(path.extname(__filename))
+
+console.log(path.parse(__filename))
