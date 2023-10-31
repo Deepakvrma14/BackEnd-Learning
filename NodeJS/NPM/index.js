b7102839aab42164a17f9c95968f616dc150cf69
Deepakvrma14 <deepakverma1482003@gmail.com>
2023-11-01 00:58:49 +0530

    modified 3 files in NPM

--- NodeJS/NPM/index.js
+++ NodeJS/NPM/index.js
@@ -1,7 +1,12 @@
 const { format } = require('date-fns');
+// const { v4:uuid } = require('uuid');
+const { v4:uuid } = require('uuid');
+
+console.log('first line of code');
 
 const now = new Date();
 const formatted = format(now, 'yyyy-MM-dd');
 
 console.log(formatted);
 console.log('last line of code');
+console.log(uuid());
